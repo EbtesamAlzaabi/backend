74820c8be487c8719613d122ffbf5efda774d39d
Ebtesam obaid nasser alzaabi <ebtesamobaid123@gmail.com>
2026-04-16 13:16:40 +0400

    Add files via upload

---
+++ task1/Day3/Program.cs
@@ -0,0 +1,225 @@
+﻿#nullable disable
+namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //control statment
+
+            //task1:Positive, Negative, or Zero (
+
+
+            //Console.WriteLine("Please Enter a number");
+            //int num= Convert.ToInt32(Console.ReadLine());
+
+            //if (num > 0)
+            //{
+            //    Console.WriteLine("positive");
+            //}
+            //else if (num < 0)
+            //{
+            //    Console.WriteLine("negative");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Zero");
+            //}
+
+
+
+            //task2:Even or Odd 
+
+            //Console.WriteLine("Please Enter a number");
+            //int num1= Convert.ToInt32(Console.ReadLine());
+
+            //if (num1 % 2 == 0)
+            //{
+            //    Console.WriteLine("Even Number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd Number");
+
+            //}
+
+
+            //task3:Student Grade System 
+
+            //Console.WriteLine("Please enter a student score");
+            //int score= Convert.ToInt32(Console.ReadLine());
+
+            //if (score >= 90 && score <= 100)
+            //{
+            //    Console.WriteLine("Excellent");
+            //}
+            //else if (score >= 75)
+            //{
+            //    Console.WriteLine("Very Good");
+            //}
+            //else if (score >= 60)
+            //{
+            //    Console.WriteLine("Good");
+            //}
+            //else if (score >= 50)
+            //{
+            //    Console.WriteLine("Pass");
+            //}
+            //else if (score <= 0)
+            //{
+            //    Console.WriteLine("Fail");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Score");
+            //}
+
+
+            // Task 4 – Simple Login System
+
+            //Console.WriteLine("Enter username: ");
+            //string username = Console.ReadLine();
+
+            //Console.WriteLine("Enter password: ");
+            //string password = Console.ReadLine();
+
+            //if (username == "admin" && password == "1234")
+            //{
+            //    Console.WriteLine("Login Successful");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Username or Password");
+            //}
+
+
+            // Task 5 – Simple ATM System
+
+            //int balance = 1000;
+
+            //Console.Write("Enter withdrawal amount: ");
+            //int amount = Convert.ToInt32(Console.ReadLine());
+
+            //if (amount <= 0)
+            //{
+            //    Console.WriteLine("Invalid Amount");
+            //}
+            //else if (amount > balance)
+            //{
+            //    Console.WriteLine("Insufficient Balance");
+            //}
+            //else
+            //{
+            //    balance -= amount;
+            //    Console.WriteLine("Withdrawal Successful");
+            //    Console.WriteLine("Remaining Balance: " + balance);
+            //}
+
+            //Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
+
+
+            #region Prime number:
+
+            #endregion//task 3:
+
+            //Console.WriteLine("please enter a number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //bool isPrime = true;
+
+            //if (num < 2)
+            //{
+            //    isPrime = false;
+            //}
+            //else
+            //{
+
+            //    for (int i = 2; i < num; i++)
+            //    {
+            //        if (num % i == 0)
+            //        {
+            //            isPrime = false;
+            //            break;
+            //        }
+            //    }
+            //}
+
+
+            //if (isPrime)
+            //{
+            //    Console.WriteLine($"{num} is a prime number ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{num} is not  a prime number");
+            //}
+
+
+
+            //task4:
+
+            //Console.WriteLine("please enter any number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //while (num !=5)
+            //{
+            //    Console.WriteLine("Incorect number");
+            //    num = Convert.ToInt32(Console.ReadLine());
+
+            //}
+
+            //////////////////////////////////////////////////
+            ///task randome :
+            ///
+            //Random random = new Random();
+            //int num = 0;
+
+
+            //while (num != 5)
+            //{
+
+            //    num = random.Next(1, 11);
+
+            //    if (num != 5)
+            //    {
+
+            //        Console.WriteLine($"Incorrect Number: {num}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Correct Number: {num}! Done.");
+            //    }
+            //}
+
+
+            ///////////////////////////////
+            ///user writ 3 time only:
+            ///
+            //int counter = 1;
+
+            //while (counter <= 3)
+            //{
+            //    Console.WriteLine($"Attempt {counter}: Please Enter any number");
+            //    int num = Convert.ToInt32(Console.ReadLine());
+
+            //    if (num == 3)
+            //    {
+            //        Console.WriteLine("Correct Number");
+            //    }
+
+            //    counter++;
+            //}
+
+            //////////////////////////////////////////////////////
+            ///DO loop:
+            ///
+
+            //////////////////////////////////////////////////////
+            ///Foreach:
+            ///
+
+
+        }
+    }
+}
