f453208c9e510d5e47bec1ab8b42a24748beeb57
Ebtesam obaid nasser alzaabi <ebtesamobaid123@gmail.com>
2026-05-04 12:39:48 +0400

    //Recursion function
    //Recursion function

---
+++ Soultion21/Recursion/Soultion21/Program.cs
@@ -0,0 +1,16 @@
+﻿// Recursion function
+static void Hello()
+{
+    Console.WriteLine("please enter your age");
+    int age = Convert.ToInt32(Console.ReadLine());
+    if (age > 18)
+    {
+        Hello();
+    }
+    else
+        return;
+}
+static void Main(string[] args)
+{
+    Hello();
+}
